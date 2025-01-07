7f240c7ba4cdcec53593eef055878862e9ee2038
Haruka Takahashi <bichonharu5@gmail.com>
2025-01-07 15:42:28 +0900

    記事を削除できるようにした。（挙動がうまくいかず未解決）

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import Rails from "./node_modules/@rails/ujs";
+Rails.start();
 import "@hotwired/turbo-rails"
 import "controllers"
+
